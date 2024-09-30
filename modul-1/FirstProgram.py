37cfd4f84771f148f18149d9ed68d972fd234c9d
nayeem98746 <sabbirahmednayeem941@gmail.com>
2024-09-30 07:41:57 +0600

    changes

---
+++ modul-1/FirstProgram.py
@@ -0,0 +1,84 @@
+print(23)
+
+name = "John"
+
+# print(name)
+a="bal bata"
+x=f"srk TuR = {a}"
+print(x)
+
+name = "John"
+
+age ="23"
+
+# print(name + age)
+
+name = "John"
+age = 23
+price = 10.5
+
+
+# print(type(name))
+# print(type(price))
+
+if(age < 18) :
+    print("You are an madafaker")
+else:
+    print("You are a kid")
+
+
+a = None
+
+print(type(a))
+
+
+A , b = 10, 20
+
+Tex = "@"
+
+# print(A*Tex*b)
+
+a = 19
+b = 10
+
+# print(a // b)
+
+# take input from user
+
+useForIntpu = input("name: ")
+print(useForIntpu)
+
+myage= int(input("Enter your age: "))
+print(myage)
+bestPrice = float(input("Enter the price: "))
+print(bestPrice)
+
+
+# condition for python 
+
+forAdult = 18
+
+if (forAdult > 8):
+    print("You are an adult")
+elif (forAdult == 18):
+    print("You are a kid")
+else :
+    print("You are a baby")  
+
+light = input("Enter the light color: ")
+
+if (light == "red"):
+    print("Stop")
+elif (light == "green"):
+    print("Go madafaker")
+else: 
+    print("Slow down")
+
+examMarks = int(input("Enter your exam marks: "))
+
+if(examMarks > 85):
+    print("You are a genius")
+elif(examMarks > 75):
+    print("you are a madaerbord")
+else:
+    print("You are a madafaker")
