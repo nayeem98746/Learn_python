ada32348ea56a15876fe194a6d379b9ca15f3f4d
nayeem98746 <sabbirahmednayeem941@gmail.com>
2024-10-11 17:40:24 +0600

    lecture2

--- modul-1/lecture3.py
+++ modul-1/lecture3.py
@@ -31,9 +31,9 @@ tupleList = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 mylist = []
 
-# movies1 = mylist.append(input("Enter your movie: "))
-# movies2 = mylist.append(input("Enter your movie: "))
-# movies3 = mylist.append(input("Enter your movie: "))
+movies1 = mylist.append(input("Enter your movie: "))
+movies2 = mylist.append(input("Enter your movie: "))
+movies3 = mylist.append(input("Enter your movie: "))
 
 # print(mylist)
 
@@ -46,3 +46,35 @@ dictionary = {
     "age": 18
 }
 print(type(dictionary))
+
+
+collection = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , "hellow", "world"}
+collection.add("nayeem")
+collection.remove(4)
+print(collection)
+print(len(collection))
+
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+
+print(set1.union(set2))
+print(set1.intersection(set2))
+
+
+marks = {}
+
+x = int(input("Enter your ph: "))
+marks.update({"physics": x})
+
+x = int(input("Enter your ph: "))
+marks.update({"math": x})
+
+x = int(input("Enter your ph: "))
+marks.update({"bio": x})
+
+print(marks)
+
+values = {9 , 9.0}
+
+print(values)
